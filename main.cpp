4b6898052d6d8e7de87a984a34882c53bbe00102
cfif <s_filip@mail.ru>
2024-06-14 13:03:27 +0400

    add class drone

--- main.cpp
+++ main.cpp
@@ -1,1 +1,110 @@
-// first main.cpp
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+class drone
+{
+public:
+  void command()
+  {
+    cout << "Введите команду (m - движение вперёд, r - поворот, h - изменение высоты): ";
+    cin >> com;
+  }
+
+  char getCom()
+  {
+    return com;
+  }
+
+  void printCurrentCoords()
+  {
+    cout << "Текущие координаты дрона: " << "(" << x << ", " << y << ", " << z << ")" << " Текущий угол поворота дрона: " << angle << " градусов" << endl;
+  }
+
+  void move(int distance)
+  {
+    double radians = angle * 3.14 / 180.0;
+
+    double sine = sin(radians);
+    double cosine = cos(radians);
+
+    double x1 = distance * cosine;
+    double y1 = distance * sine;
+
+    if (z != 0)
+    {
+      x = x + x1;
+      y = y + y1;
+    }
+  }
+
+  void turn(int _angle)
+  {
+    angle = _angle;
+  }
+
+  void heightChange(int _rise)
+  {
+    z = z + _rise;
+
+    if (z < 0)
+    {
+      z = 0;
+    }
+  }
+
+private:
+  char com;
+  double x = 0;
+  double y = 0;
+  double z = 0;
+  double angle = 0;
+};
+
+int main()
+{
+  drone copter;
+
+  while (true)
+  {
+    copter.command();
+    char command = copter.getCom();
+
+    if (command == 'm')
+    {
+      int _distance;
+      cout << "Введите расстояние: ";
+      cin >> _distance;
+      copter.move(_distance);
+      copter.printCurrentCoords();
+    }
+    else if (command == 'r')
+    {
+      int _angle;
+      cout << "Введите угол поворота: ";
+      cin >> _angle;
+      copter.turn(_angle);
+      copter.printCurrentCoords();
+    }
+    else if (command == 'h')
+    {
+      int _rise;
+      cout << "Введите изменение высоты: ";
+      cin >> _rise;
+      copter.heightChange(_rise);
+      copter.printCurrentCoords();
+    }
+    else if (command == 'q')
+    {
+      break;
+    }
+    else
+    {
+      cout << "Неизвестная команда. Попробуйте ещё раз." << endl;
+    }
+  }
+
+  return 0;
+}
